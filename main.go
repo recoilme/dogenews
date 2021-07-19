3ed18ece01a5e7c3f76472852ddc1e929b185859
recoilme <vadim-kulibaba@yandex.ru>
2021-07-19 15:41:57 +0300

    next ver

--- main.go
+++ main.go
@@ -23,7 +23,7 @@ import (
 )
 
 var (
-	ver = 2 //v0.0.2
+	ver = 3 //v0.0.3
 	//params
 	address  = flag.String("address", ":80", "address to listen on (default: :80)")
 	dbFile   = flag.String("dbfile", "db.db", "database file (main)")
